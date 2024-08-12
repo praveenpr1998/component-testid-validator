c083911c46e69b92486ce6ce17d161656b1e944d
p0t02gz <praveen.raj.thiagaraju@walmart.com>
2024-08-12 11:06:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# Component TestID Checker
+
+` Component TestID Checker` is a command-line tool that checks for the presence of `testID` attributes in React, React Native, and Angular components. This tool helps ensure that all interactive elements in your project have the necessary `testID` or also can detect multiple attributes for testing purposes.
+
+## Installation
+
+You can install the package globally using npm:
+
+```bash
+npm install -g component-testid-checker
+```
+
+## Usage
+
+Run the tool using the command:
+
+```
+  check-test-ids --config path/to/your-config.json
+```
+
+or add it in the scripts of your package.json file
+
+```
+  "check-test-ids": "check-test-ids --config config.json"
+```
+
+and run
+
+```
+   npm run check-test-ids
+```
+
+## Screenshots
+
+![Alt Text](https://raw.githubusercontent.com/praveenpr1998/assetsfolder/main/failedtests.png)
+
+![Alt Text](https://raw.githubusercontent.com/praveenpr1998/assetsfolder/main/passedtests.png)
+
+## Configuration
+
+The tool requires a configuration file to specify various options. The configuration file should be a JSON file with the following structure:
+
+## React Native
+
+```
+ {
+  "directoryToCheck": "src",
+  "testIdAttributes": ["testID"],
+  "extensions": "js,jsx,tsx,ts",
+  "outputFormat": "text",
+  "excludePattern": "",
+  "nonInteractiveElements": ["View", "Text", "Image"],
+  "internalElementPattern": "^_",
+  "interactiveElements": [
+    "Button",
+    "TouchableOpacity",
+    "TextInput",
+    "ScrollView",
+    "FlatList"
+  ],
+  "dynamicTestIdFunctions": ["getTestID"],
+  "chalkConfig": {
+    "componentName": "red",
+    "lineNumber": "yellow",
+    "filePath": "blue",
+    "attributeName": "green",
+    "totalMissingCount": "magenta"
+  }
+}
+
+```
+
+## React
+
+```
+{
+  "directoryToCheck": "src",
+  "testIdAttributes": ["data-testid"],
+  "extensions": "js,jsx,tsx,ts",
+  "outputFormat": "text",
+  "excludePattern": "",
+  "nonInteractiveElements": ["div", "span", "img"],
+  "internalElementPattern": "^_",
+  "interactiveElements": [
+    "button",
+    "a",
+    "input",
+    "select",
+    "textarea"
+  ],
+  "dynamicTestIdFunctions": ["getTestID"],
+  "chalkConfig": {
+    "componentName": "red",
+    "lineNumber": "yellow",
+    "filePath": "blue",
+    "attributeName": "green",
+    "totalMissingCount": "magenta"
+  }
+}
+
+
+```
+
+### Configuration Options
+
+- **`directoryToCheck` (string)**: The directory to search for component files. Default is `'src'`.
+- **`testIdAttributes` (array of strings)**: The list of `testID` attributes to check for. Default is `['testID']`.
+- **`extensions` (string)**: The file extensions to check, separated by commas. Default is `'js,jsx,tsx,ts'`.
+- **`outputFormat` (string)**: The output format for logs, either `'text'` or `'json'`. Default is `'text'`.
+- **`excludePattern` (string)**: A glob pattern for files to exclude from the check.
+- **`nonInteractiveElements` (array of strings)**: A list of non-interactive element names to exclude from the check.
+- **`internalElementPattern` (string)**: A regex pattern to identify internal element names that should be excluded from the check.
+- **`interactiveElements` (array of strings)**: A list of interactive element names to include in the check.
+- **`dynamicTestIdFunctions` (array of strings)**: A list of function names that dynamically set `testID` attributes.
+- **`chalkConfig` (object)**: Configuration for chalk colors. Each key represents a log element, and the value is a string specifying the chalk styles.
+
+## Example
+
+To run the tool with a custom configuration file, use:
+
+```
+check-test-ids --config ./my-custom-config.json
+```
+
+## Example Output
+
+If the tool finds elements missing testID attributes, it will log warnings:
+
+```
+Warning: <Button> in src/components/MyButton.js at line 15 does not have a testID attribute.
+Warning: <TouchableOpacity> in src/screens/HomeScreen.js at line 27 does not have a testID attribute.
+Total elements missing testID: 2
+```
+
+If all elements have testID attributes, it will display a success message:
+
+```
+All components have testID attributes.
+```
+
+## Additional Information
+
+- **`Non-Interactive Elements`**: The tool will skip checking for testID attributes on elements that are purely decorative or non-interactive.
+- **`Internal Elements`**: Elements meant to be private or internal to a component will also be skipped.
+- **`Elements with Stable Semantic Selectors`**: If an element can be reliably selected using semantic HTML, ARIA roles, or stable class names, prefer those over adding an extra testID..
+
+## Feedback
+
+If you have any feedback, please reach out to me at praveenpr1998@gmail.com
+
+### Contribution
+
+Contributions are welcome! For major changes, please open an issue first to discuss what you would like to change.
+
+By Order of the Peaky Blinders!
---
+++ check-test-ids.js
@@ -0,0 +1,163 @@
+#!/usr/bin/env node
+
+const glob = require("glob");
+const fs = require("fs");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const chalk = require("chalk");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+// Use yargs to parse command-line arguments
+const argv = yargs(hideBin(process.argv)).argv;
+
+// Load configuration
+const configPath = argv.config;
+
+if (!configPath) {
+  console.error(chalk.red(`Please provide a config file path`));
+  process.exit(1);
+}
+
+try {
+  config = require(configPath);
+} catch (err) {
+  console.error(
+    chalk.red(`Error loading configuration file ${configPath}: ${err.message}`)
+  );
+  process.exit(1);
+}
+
+const directoryToCheck = config.directoryToCheck || "src";
+const testIdAttributes = config.testIdAttributes || ["testID"];
+const extensions = config.extensions || "js,jsx,tsx,ts";
+const outputFormat = config.outputFormat || "text";
+const excludePattern = config.excludePattern || "";
+const dynamicTestIdFunction = config.dynamicTestIdFunction || "getTestID";
+
+const nonInteractiveElements = new Set(config.nonInteractiveElements);
+const internalElementPattern = new RegExp(config.internalElementPattern);
+const interactiveElements = new Set(config.interactiveElements);
+
+const colors = config.colors || {
+  componentName: "red",
+  lineNumber: "blue",
+  fileLocation: "green",
+  attributeName: "yellow",
+  totalMissing: "magenta",
+};
+
+const globPattern = `${directoryToCheck}/**/*.{${extensions
+  .split(",")
+  .join(",")}}`;
+
+let missingTestIdCount = 0;
+
+function logWarning(elementName, filePath, lineNumber, missingAttributes) {
+  const coloredComponentName = chalk[colors.componentName](elementName);
+  const coloredFileLocation = chalk[colors.fileLocation](filePath);
+  const coloredLineNumber = chalk[colors.lineNumber](lineNumber);
+  const coloredAttributeName = chalk[colors.attributeName](
+    missingAttributes.join(", ")
+  );
+
+  const coloredMessage = chalk.yellow.bold(
+    `Warning: <${coloredComponentName}> in ${coloredFileLocation} at line ${coloredLineNumber} does not have ${coloredAttributeName} attributes.`
+  );
+  const JSONMessage = `No ${missingAttributes.join(", ")} attribute`;
+
+  if (outputFormat === "json") {
+    console.log(
+      JSON.stringify({ elementName, filePath, lineNumber, JSONMessage })
+    );
+  } else {
+    console.log(coloredMessage);
+  }
+}
+
+function checkTestIdInFile(filePath) {
+  try {
+    const code = fs.readFileSync(filePath, "utf-8");
+    const ast = parser.parse(code, {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+      allowImportExportEverywhere: true,
+      allowAwaitOutsideFunction: true,
+    });
+
+    traverse(ast, {
+      JSXOpeningElement(path) {
+        const elementName = path.node.name.name;
+
+        if (nonInteractiveElements.has(elementName)) return;
+        if (internalElementPattern.test(elementName)) return;
+        if (!interactiveElements.has(elementName)) return;
+
+        const attributes = path.node.attributes;
+        const missingAttributes = testIdAttributes.filter((attr) => {
+          const hasTestId = attributes.some(
+            (attribute) =>
+              attribute.type === "JSXAttribute" && attribute.name.name === attr
+          );
+
+          const hasDynamicTestId = attributes.some((attribute) => {
+            if (attribute.type === "JSXSpreadAttribute") {
+              const argument = attribute.argument;
+              if (
+                argument.callee &&
+                argument.callee.type === "Identifier" &&
+                argument.callee.name === dynamicTestIdFunction
+              ) {
+                return true;
+              }
+            }
+            return false;
+          });
+
+          return !(hasTestId || hasDynamicTestId);
+        });
+
+        if (missingAttributes.length > 0) {
+          const lineNumber = path.node.loc.start.line;
+          missingTestIdCount++;
+          logWarning(elementName, filePath, lineNumber, missingAttributes);
+        }
+      },
+    });
+  } catch (err) {
+    console.error(chalk.red(`Error parsing file ${filePath}: ${err.message}`));
+  }
+}
+
+function checkTestIdsInDirectory(directoryPath) {
+  glob(globPattern, { ignore: excludePattern }, (err, files) => {
+    if (err) {
+      console.error(chalk.red("Error finding files:", err));
+      return;
+    }
+
+    files.forEach((file) => {
+      checkTestIdInFile(file);
+    });
+  });
+}
+
+process.on("exit", () => {
+  if (missingTestIdCount === 0) {
+    console.log(
+      chalk.green.bold("✨ All components have testID attributes added! ✨")
+    );
+  } else {
+    console.log("\n");
+    console.log(
+      chalk[colors.totalMissing](
+        `Total elements missing ${testIdAttributes.join(
+          ", "
+        )}: ${missingTestIdCount}`
+      )
+    );
+    console.log("\n");
+  }
+});
+
+checkTestIdsInDirectory(directoryToCheck);
